933bae8ad9a83cca1266437d81863bf9b1672dea
Leonid8000 <leonidzp8000@ukr.net>
2019-10-13 02:10:23 +0300

    edit phone bag

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,11 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\User;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
-use Illuminate\Support\Facades\Validator;
 
 class ProfileController extends Controller
 {
